a7c2a3f9c0b8ec4809742d3a1176ddf95c24e558
Kartik Gohil <kartikgohil@360incemail.com>
2026-08-07 17:09:22 +0530

    fix: add babel.config.js and SafeAreaProvider to fix infinite reload on Expo Go

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { RootNavigator } from "./src/navigation";
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
 
