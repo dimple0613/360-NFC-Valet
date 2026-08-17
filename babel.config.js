7094b62b42d5a2fefcc7c0acca8510c3fff4f846
Dimple <dimple@360incemail.com>
2026-08-17 12:24:06 +0530

    fix: scope babel font-alias plugin to src/ only via overrides to prevent transforming node_modules

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,11 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["./babel-plugin-font-alias.js"],
+    overrides: [
+      {
+        test: /[\\/]src[\\/]/,
+        plugins: ["./babel-plugin-font-alias.js"],
+      },
+    ],
   };
 };
